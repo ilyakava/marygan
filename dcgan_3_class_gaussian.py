cb8d727369d5dbb3d5c536e3fb19086bdf8fa6db
Ilya Kavalerov <ilyakavalerov@gmail.com>
2019-09-10 15:50:49 -0400

    Remove verbose comments

--- dcgan_3_class_gaussian.py
+++ dcgan_3_class_gaussian.py
@@ -37,41 +37,6 @@ torch.manual_seed(manualSeed)
 vis = visdom.Visdom()
 last_itr_visuals = []
 
-######################################################################
-# Inputs
-# ------
-# 
-# Let’s define some inputs for the run:
-# 
-# -  **dataroot** - the path to the root of the dataset folder. We will
-#    talk more about the dataset in the next section
-# -  **workers** - the number of worker threads for loading the data with
-#    the DataLoader
-# -  **batch_size** - the batch size used in training. The DCGAN paper
-#    uses a batch size of 128
-# -  **image_size** - the spatial size of the images used for training.
-#    This implementation defaults to 64x64. If another size is desired,
-#    the structures of D and G must be changed. See
-#    `here <https://github.com/pytorch/examples/issues/70>`__ for more
-#    details
-# -  **nc** - number of color channels in the input images. For color
-#    images this is 3
-# -  **nz** - length of latent vector
-# -  **ngf** - relates to the depth of feature maps carried through the
-#    generator
-# -  **ndf** - sets the depth of feature maps propagated through the
-#    discriminator
-# -  **num_epochs** - number of training epochs to run. Training for
-#    longer will probably lead to better results but will also take much
-#    longer
-# -  **lr** - learning rate for training. As described in the DCGAN paper,
-#    this number should be 0.0002
-# -  **beta1** - beta1 hyperparameter for Adam optimizers. As described in
-#    paper, this number should be 0.5
-# -  **ngpu** - number of GPUs available. If this is 0, code will run in
-#    CPU mode. If this number is greater than 0 it will run on that number
-#    of GPUs
-# 
 
 # Root directory for dataset
 legend = ['fake', 'data1', 'data2']
@@ -140,39 +105,8 @@ n_classes = len(mus)+1 # including fake
 critic_iters = 1
 labeled_iters = 1
 
-
 ######################################################################
-# Data
-# ----
-#
-# In this tutorial we will use the `Celeb-A Faces
-# dataset <http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html>`__ which can
-# be downloaded at the linked site, or in `Google
-# Drive <https://drive.google.com/drive/folders/0B7EVK8r0v71pTUZsaXdaSnZBZzg>`__.
-# The dataset will download as a file named *img_align_celeba.zip*. Once
-# downloaded, create a directory named *celeba* and extract the zip file
-# into that directory. Then, set the *dataroot* input for this notebook to
-# the *celeba* directory you just created. The resulting directory
-# structure should be:
-# 
-# ::
-# 
-#    /path/to/celeba
-#        -> img_align_celeba  
-#            -> 188242.jpg
-#            -> 173822.jpg
-#            -> 284702.jpg
-#            -> 537394.jpg
-#               ...
-# 
-# This is an important step because we will be using the ImageFolder
-# dataset class, which requires there to be subdirectories in the
-# dataset’s root folder. Now, we can create the dataset, create the
-# dataloader, set the device to run on, and finally visualize some of the
-# training data.
-# 
 
-# We can use an image folder dataset the way we have it setup.
 # Create a isotropic dataset
 n_examples = 50000
 
@@ -194,34 +128,13 @@ datalabels = datalabels[perm]
 dataloader = torch.tensor(dataloader, dtype=torch.float)
 datalabels = torch.tensor(datalabels, dtype=torch.long)
 
-
-
-
-
 # Decide which device we want to run on
 device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
 ######################################################################
 # Implementation
 # --------------
-# 
-# With our input parameters set and the dataset prepared, we can now get
-# into the implementation. We will start with the weigth initialization
-# strategy, then talk about the generator, discriminator, loss functions,
-# and training loop in detail.
-# 
-# Weight Initialization
-# ~~~~~~~~~~~~~~~~~~~~~
-# 
-# From the DCGAN paper, the authors specify that all model weights shall
-# be randomly initialized from a Normal distribution with mean=0,
-# stdev=0.2. The ``weights_init`` function takes an initialized model as
-# input and reinitializes all convolutional, convolutional-transpose, and
-# batch normalization layers to meet this criteria. This function is
-# applied to the models immediately after initialization.
-# 
 
-# custom weights initialization called on netG and netD
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
@@ -229,37 +142,6 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
-
-
-######################################################################
-# Generator
-# ~~~~~~~~~
-# 
-# The generator, :math:`G`, is designed to map the latent space vector
-# (:math:`z`) to data-space. Since our data are images, converting
-# :math:`z` to data-space means ultimately creating a RGB image with the
-# same size as the training images (i.e. 3x64x64). In practice, this is
-# accomplished through a series of strided two dimensional convolutional
-# transpose layers, each paired with a 2d batch norm layer and a relu
-# activation. The output of the generator is fed through a tanh function
-# to return it to the input data range of :math:`[-1,1]`. It is worth
-# noting the existence of the batch norm functions after the
-# conv-transpose layers, as this is a critical contribution of the DCGAN
-# paper. These layers help with the flow of gradients during training. An
-# image of the generator from the DCGAN paper is shown below.
-#
-# .. figure:: /_static/img/dcgan_generator.png
-#    :alt: dcgan_generator
-#
-# Notice, the how the inputs we set in the input section (*nz*, *ngf*, and
-# *nc*) influence the generator architecture in code. *nz* is the length
-# of the z input vector, *ngf* relates to the size of the feature maps
-# that are propagated through the generator, and *nc* is the number of
-# channels in the output image (set to 3 for RGB images). Below is the
-# code for the generator.
-# 
-
-# Generator Code
 
 class Generator(nn.Module):
     # https://github.com/igul222/improved_wgan_training/blob/master/gan_toy.py
@@ -283,13 +165,7 @@ class Generator(nn.Module):
 
     def forward(self, input):
         return self.main(input)
-######################################################################
-# Now, we can instantiate the generator and apply the ``weights_init``
-# function. Check out the printed model to see how the generator object is
-# structured.
-# 
 
-# Create the generator
 netG = Generator(ngpu).to(device)
 
 # Handle multi-gpu if desired
@@ -300,31 +176,7 @@ if (device.type == 'cuda') and (ngpu > 1):
 #  to mean=0, stdev=0.2.
 netG.apply(weights_init)
 
-# Print the model
 print(netG)
-
-
-######################################################################
-# Discriminator
-# ~~~~~~~~~~~~~
-# 
-# As mentioned, the discriminator, :math:`D`, is a binary classification
-# network that takes an image as input and outputs a scalar probability
-# that the input image is real (as opposed to fake). Here, :math:`D` takes
-# a 3x64x64 input image, processes it through a series of Conv2d,
-# BatchNorm2d, and LeakyReLU layers, and outputs the final probability
-# through a Sigmoid activation function. This architecture can be extended
-# with more layers if necessary for the problem, but there is significance
-# to the use of the strided convolution, BatchNorm, and LeakyReLUs. The
-# DCGAN paper mentions it is a good practice to use strided convolution
-# rather than pooling to downsample because it lets the network learn its
-# own pooling function. Also batch norm and leaky relu functions promote
-# healthy gradient flow which is critical for the learning process of both
-# :math:`G` and :math:`D`.
-# 
-
-#########################################################################
-# Discriminator Code
 
 class Discriminator(nn.Module):
     # https://github.com/igul222/improved_wgan_training/blob/master/gan_toy.py
@@ -352,13 +204,6 @@ class Discriminator(nn.Module):
         else:
             return self.net(input)
 
-
-######################################################################
-# Now, as with the generator, we can create the discriminator, apply the
-# ``weights_init`` function, and print the model’s structure.
-# 
-
-# Create the Discriminator
 netD = Discriminator(ngpu).to(device)
 
 # Handle multi-gpu if desired
@@ -369,45 +214,13 @@ if (device.type == 'cuda') and (ngpu > 1):
 #  to mean=0, stdev=0.2.
 netD.apply(weights_init)
 
-# Print the model
 print(netD)
 
 
 ######################################################################
 # Loss Functions and Optimizers
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 
-# With :math:`D` and :math:`G` setup, we can specify how they learn
-# through the loss functions and optimizers. We will use the Binary Cross
-# Entropy loss
-# (`BCELoss <https://pytorch.org/docs/stable/nn.html#torch.nn.BCELoss>`__)
-# function which is defined in PyTorch as:
-# 
-# .. math:: \ell(x, y) = L = \{l_1,\dots,l_N\}^\top, \quad l_n = - \left[ y_n \cdot \log x_n + (1 - y_n) \cdot \log (1 - x_n) \right]
-# 
-# Notice how this function provides the calculation of both log components
-# in the objective function (i.e. :math:`log(D(x))` and
-# :math:`log(1-D(G(z)))`). We can specify what part of the BCE equation to
-# use with the :math:`y` input. This is accomplished in the training loop
-# which is coming up soon, but it is important to understand how we can
-# choose which component we wish to calculate just by changing :math:`y`
-# (i.e. GT labels).
-# 
-# Next, we define our real label as 1 and the fake label as 0. These
-# labels will be used when calculating the losses of :math:`D` and
-# :math:`G`, and this is also the convention used in the original GAN
-# paper. Finally, we set up two separate optimizers, one for :math:`D` and
-# one for :math:`G`. As specified in the DCGAN paper, both are Adam
-# optimizers with learning rate 0.0002 and Beta1 = 0.5. For keeping track
-# of the generator’s learning progression, we will generate a fixed batch
-# of latent vectors that are drawn from a Gaussian distribution
-# (i.e. fixed_noise) . In the training loop, we will periodically input
-# this fixed_noise into :math:`G`, and over the iterations we will see
-# images form out of the noise.
-# 
 
-
-# Losses
 bce = nn.BCELoss()
 nll = nn.NLLLoss()
 def criterion(x,y):
@@ -456,74 +269,9 @@ fake_label = 0
 optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
 optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
 
-
 ######################################################################
 # Training
 # ~~~~~~~~
-# 
-# Finally, now that we have all of the parts of the GAN framework defined,
-# we can train it. Be mindful that training GANs is somewhat of an art
-# form, as incorrect hyperparameter settings lead to mode collapse with
-# little explanation of what went wrong. Here, we will closely follow
-# Algorithm 1 from Goodfellow’s paper, while abiding by some of the best
-# practices shown in `ganhacks <https://github.com/soumith/ganhacks>`__.
-# Namely, we will “construct different mini-batches for real and fake”
-# images, and also adjust G’s objective function to maximize
-# :math:`logD(G(z))`. Training is split up into two main parts. Part 1
-# updates the Discriminator and Part 2 updates the Generator.
-# 
-# **Part 1 - Train the Discriminator**
-# 
-# Recall, the goal of training the discriminator is to maximize the
-# probability of correctly classifying a given input as real or fake. In
-# terms of Goodfellow, we wish to “update the discriminator by ascending
-# its stochastic gradient”. Practically, we want to maximize
-# :math:`log(D(x)) + log(1-D(G(z)))`. Due to the separate mini-batch
-# suggestion from ganhacks, we will calculate this in two steps. First, we
-# will construct a batch of real samples from the training set, forward
-# pass through :math:`D`, calculate the loss (:math:`log(D(x))`), then
-# calculate the gradients in a backward pass. Secondly, we will construct
-# a batch of fake samples with the current generator, forward pass this
-# batch through :math:`D`, calculate the loss (:math:`log(1-D(G(z)))`),
-# and *accumulate* the gradients with a backward pass. Now, with the
-# gradients accumulated from both the all-real and all-fake batches, we
-# call a step of the Discriminator’s optimizer.
-# 
-# **Part 2 - Train the Generator**
-# 
-# As stated in the original paper, we want to train the Generator by
-# minimizing :math:`log(1-D(G(z)))` in an effort to generate better fakes.
-# As mentioned, this was shown by Goodfellow to not provide sufficient
-# gradients, especially early in the learning process. As a fix, we
-# instead wish to maximize :math:`log(D(G(z)))`. In the code we accomplish
-# this by: classifying the Generator output from Part 1 with the
-# Discriminator, computing G’s loss *using real labels as GT*, computing
-# G’s gradients in a backward pass, and finally updating G’s parameters
-# with an optimizer step. It may seem counter-intuitive to use the real
-# labels as GT labels for the loss function, but this allows us to use the
-# :math:`log(x)` part of the BCELoss (rather than the :math:`log(1-x)`
-# part) which is exactly what we want.
-# 
-# Finally, we will do some statistic reporting and at the end of each
-# epoch we will push our fixed_noise batch through the generator to
-# visually track the progress of G’s training. The training statistics
-# reported are:
-# 
-# -  **Loss_D** - discriminator loss calculated as the sum of losses for
-#    the all real and all fake batches (:math:`log(D(x)) + log(D(G(z)))`).
-# -  **Loss_G** - generator loss calculated as :math:`log(D(G(z)))`
-# -  **D(x)** - the average output (across the batch) of the discriminator
-#    for the all real batch. This should start close to 1 then
-#    theoretically converge to 0.5 when G gets better. Think about why
-#    this is.
-# -  **D(G(z))** - average discriminator outputs for the all fake batch.
-#    The first number is before D is updated and the second number is
-#    after D is updated. These numbers should start near 0 and converge to
-#    0.5 as G gets better. Think about why this is.
-# 
-# **Note:** This step might take a while, depending on how many epochs you
-# run and if you removed some data from the dataset.
-# 
 
 def decimate_ts(ts, ds):
     win = np.ones(ds) / float(ds)
@@ -542,8 +290,6 @@ def decimate(y, ds):
             return decimate_ts(y, ds)
     else:
         return y
-
-# Training Loop
 
 # Lists to keep track of progress
 img_list = []
@@ -731,23 +477,3 @@ while True:
         #     last_itr_visuals.append(vis.line(decimate(perf, ds), perftime[::ds], opts={'title': 'iters per second'}))
         
     iters += 1
-
-
-
-
-######################################################################
-# Where to Go Next
-# ----------------
-# 
-# We have reached the end of our journey, but there are several places you
-# could go from here. You could:
-# 
-# -  Train for longer to see how good the results get
-# -  Modify this model to take a different dataset and possibly change the
-#    size of the images and the model architecture
-# -  Check out some other cool GAN projects
-#    `here <https://github.com/nashory/gans-awesome-applications>`__
-# -  Create GANs that generate
-#    `music <https://deepmind.com/blog/wavenet-generative-model-raw-audio/>`__
-# 
-
